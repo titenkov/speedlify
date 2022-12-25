5e5111ffc3e61b02d3a9e8296e1b6979da75bec7
Pavel Titenkov <pavel@titenkov.com>
2022-12-25 17:10:28 +0100

    🪄 override for titenkov

---
+++ _data/sites/titenkov.js
@@ -0,0 +1,10 @@
+module.exports = {
+	description: "My websites",
+	options: {
+		frequency: 60 * 24, // (in minutes), 24 hours
+	},
+	urls: [
+		"https://titenkov.com/",
+		"https://zorka.titenkov.com/",
+	]
+};
